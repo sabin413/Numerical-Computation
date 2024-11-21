53ba4d427ea17acb3bb06122867cb2b9d4d52f05
Sabin Adhikari <39281480+sabin413@users.noreply.github.com>
2024-11-21 11:46:10 -0600

    Update rotator.py

--- rotator.py
+++ rotator.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[41]:
-
+### This is a collection of 9 utility functions needed to process calculations in between the automated simulations. The main code will import this file as a python module.
+### Writing a class is not a good idea here as the functions are not all related
 
 import numpy as np
 from numpy.linalg import norm
@@ -10,11 +7,9 @@ import matplotlib.pyplot as plt
 from scipy.integrate import dblquad
 from scipy.spatial import distance
 
+# function1: a function that reads the trajectory file line by line, extracts numerical data, and calculates the bond vectors
 
-# In[42]:
-
-
-def get_data(data_path, r_inner = 10, r_outer = 50):
+def get_data(data_path, r_inner = 10, r_outer = 50):   
     
     '''reads data, and outputs the bond vectors'''
     
@@ -46,8 +41,6 @@ def get_data(data_path, r_inner = 10, r_outer = 50):
         if lst1[1] == 1:           # correct it later
             bond_pairs_data.append([lst1[2], lst1[3]]) # bond pairs
             
-    
-    
     atoms_data = pos_and_vel_data.split("Velocities")
     atoms_data = atoms_data[0]
     atoms_string_splitted = atoms_data.split("Atoms # full")
@@ -89,9 +82,7 @@ def get_data(data_path, r_inner = 10, r_outer = 50):
     
     return bond_vectors
 
-
-# In[43]:
-
+# function2: calculate the alignment tensor
 
 def alignment_tensor(vec_arr):
     
@@ -121,33 +112,9 @@ def alignment_tensor(vec_arr):
     
     return w_v_pair
 
-
-# In[44]:
-
-
-#- bond_vectors = get_data('data_from_sim')
-#- answer = alignment_tensor(bond_vectors)
-#- answer
-
-
-# In[45]:
-
-
-#, b, c = answer[0][0], answer[1][0], answer[2][0]
-#, v, w = answer[0][1], answer[1][1], answer[2][1]
-
-#- a, u = answer[0][0], answer[0][1]
-#- b, v = answer[1][0], answer[1][1]
-#- c, w = answer[2][0], answer[2][1]
-# = 0.6
-# = 0.45
-
-
-# In[46]:
-
-
 ### Now find cell bead positions
 # parametric eqns for an ellipsoid 
+# functions 3:
 
 def sin(x):
     return np.sin(x)
@@ -164,9 +131,7 @@ def y_cor(b, theta, phi):
 def z_cor(c, theta, phi):
     return c*cos(theta)
 
-
-# In[47]:
-
+# function 4: see docstring
 
 def p(theta, phi, a, b, c):
     '''Calculates the unnormalized probability of choosing a given point (theta, phi)
@@ -178,15 +143,7 @@ def p(theta, phi, a, b, c):
 
     return term_1 * term_2
 
-# p(1, 4, 1, 1, 1)
-
-
-# In[48]:
-
-
-#def custom_pdf(theta, phi):
-#    return p(theta, phi, a, b, c)
-
+# function 5: do sampling
 
 def metropolis_hastings_sampling(pdf, num_samples=1, num_iter=5000, proposal_std=0.5, x_range=(0, np.pi), y_range=(0, 2*np.pi)):
     samples = []
@@ -209,9 +166,7 @@ def metropolis_hastings_sampling(pdf, num_samples=1, num_iter=5000, proposal_std
 
     return samples
 
-
-# In[49]:
-
+# function 6: find minimum distance
 
 def min_distance(p1, lis):
     if lis == []:
@@ -219,6 +174,8 @@ def min_distance(p1, lis):
     distance_list = [distance.euclidean(p1, p2) for p2 in lis]
     # print(np.min(distance_list))
     return np.min(distance_list)
+
+# function 7: see docstring
 
 def create_tractor_beads(a, b, c, no_of_points,  bead_diameter = 1, overlap_threshold = 1.0):
     '''creates tractor beads on the surface of an ellipsoid '''
@@ -249,8 +206,8 @@ def create_tractor_beads(a, b, c, no_of_points,  bead_diameter = 1, overlap_thre
         final_list.append(point)
     
     return np.array(final_list)
-#print(len(final_list))
 
+# function 9: see docstring
 
 def create_cell_beads(a, b, c, center, coverage = 1, bead_diameter = 1, overlap_threshold = 0.5):
     '''creates beads on the cell surface - this code works even when the cell is ellipsoidal'''
@@ -289,45 +246,3 @@ def create_cell_beads(a, b, c, center, coverage = 1, bead_diameter = 1, overlap_
         final_list.append(point)
     
     return np.array(final_list)
-
-
-
-
-#- fig = plt.figure(figsize=(8, 6))
-#- ax = fig.add_subplot(111, projection='3d')
-
-# Unpack x, y, and z coordinates from the NumPy array
-#- x, y, z = rotated_tractor_beads_array[:, 0], rotated_tractor_beads_array[:, 1], rotated_tractor_beads_array[:, 2]
-
-#- x1, y1, z1 = rotated_cell_beads_array[:, 0], rotated_cell_beads_array[:, 1], rotated_cell_beads_array[:, 2]
-
-# Plot the points in 3D
-#- ax.scatter(x, y, z, s=1)
-#- ax.scatter(x1, y1, z1, s=1)
-# Set labels for the axes
-
-#- ax.set_xlabel('X')
-#- ax.set_ylabel('Y')
-#- ax.set_zlabel('Z')
-
-# Set limits for x, y, and z axes
-#- ax.set_xlim(-40, 40)  # Set x-axis limits
-#- ax.set_ylim(-40, 40)  # Set y-axis limits
-#- ax.set_zlim(-40, 40)  # Set z-axis limits
-
-# Set tick spacing for x, y, and z axes
-#- ax.xaxis.set_major_locator(MultipleLocator(10))  # Set x-axis tick spacing
-#- ax.yaxis.set_major_locator(MultipleLocator(10))  # Set y-axis tick spacing
-#- ax.zaxis.set_major_locator(MultipleLocator(10))  # Set z-axis tick spacing
-
-#- ax.set_box_aspect([1, 1, 1])
-
-#- ax.view_init(elev=5, azim=45)
-
-#- plt.tight_layout()
-# Show the 3D plot
-#- plt.title('3D Visualization of Points')
-#- plt.show()
-
-
-
